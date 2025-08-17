c6c1415b90574ceae054ba7fd247960d6676f051
Attila Sragli <sragli.attila@gmail.com>
2025-08-17 19:10:42 +0200

    Incremented release version

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ If [available in Hex](https://hex.pm/docs/publish), the package can be installed
 ```elixir
 def deps do
   [
-    {:anova, "~> 0.2.0"}
+    {:anova, "~> 0.3.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ANOVA.MixProject do
   def project do
     [
       app: :anova,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       description: description(),
