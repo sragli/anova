2d2a194ff09220ddcae091a675f9db4b280ce7ed
Attila Sragli <sragli.attila@gmail.com>
2025-08-23 10:19:34 +0200

    Implemented verification for Tukey's HSD using scipy.stats

--- test/anova_verification.py
+++ test/anova_verification.py
@@ -1,6 +1,10 @@
 from scipy.stats import f_oneway
+from scipy.stats import tukey_hsd
 
 groups = [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]]
 
 F, p = f_oneway(groups[0], groups[1], groups[2])
 print(F, p)
+
+tukey_res = tukey_hsd(groups[0], groups[1], groups[2])
+print(tukey_res)
